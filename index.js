71a748bf0b3eafbda20e3462e9fbc5eafbea97d8
Jaeri Park <motonimjrp@gmail.com>
2021-04-26 23:13:37 -0400

    create function createGrid()

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const grid = document.querySelector('.grid');
 const startButton = document.querySelector('#start');
 const score = document.getElementById('score');
+let squares = [];
 
-function createGrid() {}
+function createGrid() {
+  //create 100 of these elements with a for loop
+  for (let i = 0; i < 100; i++) {
+    //create element
+    const square = document.createElement('div');
+
+    //add styling to the element
+    square.classList.add('square');
+
+    //put the element into our grid
+    grid.appendChild(square);
+
+    //push it into a new squares array
+    squares.push(square);
+  }
+}
+
+createGrid();
